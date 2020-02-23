9a14102be13ebf30af98796e5be0267aebf6fb7a
harshithgudpati <jinnu95@gmail.com>
2020-02-23 14:57:36 -0600

    added file

--- Gender-count/reducer.py
+++ Gender-count/reducer.py
@@ -1,18 +1,18 @@
-s = open("02.txt","r")
-r = open("r.txt", "w")
+s = open("2.CSV","r")
+r = open("3.CSV", "w")
 
 thisKey = ""
 thisValue = 0.0
 count = {}
 
 for line in s:
-  data = line.strip().split('\t',1)
+  data = line.strip().split(',',1)
   Gender, count = data
 
   if Gender != thisKey:
     if thisKey:
       # output the last key value pair result
-      r.write(thisKey + '\t' + str(thisValue)+'\n')
+      r.write(thisKey + ',' + str(thisValue)+'\n')
 
     # start over when changing keys
     thisKey = Gender 
@@ -22,7 +22,7 @@ for line in s:
   thisValue += float(count)
 
 # output the final entry when done
-r.write(thisKey + '\t' + str(thisValue)+'\n')
+r.write(thisKey + ',' + str(thisValue)+'\n')
 
 s.close()
 r.close()
